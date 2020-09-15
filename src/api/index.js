5404d1d502f927d119e879706f46e9ce52b6a544
green-code-developer <complexcodealgorithm@gmail.com>
2020-09-15 21:08:07 +0530

    two ways of receiving data from API

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,31 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
+
         const response = await axios.get(url);
-       
-        return response;
+
+        const alternative = {
+            conformed: response.data.conformed,
+            recovered: response.data.recovered,
+            deaths: response.data.deaths,
+            lastUpdate: response.data.lastUpdate
+        }
+
+
+        
+        const { data} = await axios.get(url);        // before it was: const response = await axios.get(url);
+                                                     // response.data
+        const modifiedData = {
+            conformed: data.conformed,                // response.data.conformed
+            recovered: data.recovered,                // response.data.recovered
+            deaths: data.deaths,                      // response.data.deaths
+            lastUpdate: data.lastUpdate               // response.data.lastUpdate
+
+        }
+
+        
+
+        
     } catch (error) {
         
     }
