836cefafa372a26f082550a99efca3bb5b1f2d54
green-code-developer <complexcodealgorithm@gmail.com>
2020-09-15 21:50:42 +0530

    better way of receiving( initializing) data

--- src/api/index.js
+++ src/api/index.js
@@ -5,13 +5,13 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
 
-        const { data} = await axios.get(url);        // before it was: const response = await axios.get(url);
+        const { data:{conformed,recovered,deaths,lastUpdate} } = await axios.get(url);        // before it was: const response = await axios.get(url);
                                                      
         const modifiedData = {
-            conformed: data.conformed,                
-            recovered: data.recovered,           
-            deaths: data.deaths,                  
-            lastUpdate: data.lastUpdate               
+            conformed: conformed,                
+            recovered: recovered,           
+            deaths: deaths,                  
+            lastUpdate: lastUpdate               
 
         }
 
