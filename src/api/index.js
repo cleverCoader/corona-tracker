c1718cf5bb026e42e836b7fd7a7f076f916c314d
green-code-developer <complexcodealgorithm@gmail.com>
2020-09-15 21:46:56 +0530

    receiving data from API

--- src/api/index.js
+++ src/api/index.js
@@ -5,24 +5,13 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
 
-        const response = await axios.get(url);
-
-        const alternative = {
-            conformed: response.data.conformed,
-            recovered: response.data.recovered,
-            deaths: response.data.deaths,
-            lastUpdate: response.data.lastUpdate
-        }
-
-
-        
         const { data} = await axios.get(url);        // before it was: const response = await axios.get(url);
-                                                     // response.data
+                                                     
         const modifiedData = {
-            conformed: data.conformed,                // response.data.conformed
-            recovered: data.recovered,                // response.data.recovered
-            deaths: data.deaths,                      // response.data.deaths
-            lastUpdate: data.lastUpdate               // response.data.lastUpdate
+            conformed: data.conformed,                
+            recovered: data.recovered,           
+            deaths: data.deaths,                  
+            lastUpdate: data.lastUpdate               
 
         }
 
