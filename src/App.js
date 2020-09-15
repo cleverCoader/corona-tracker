1ffffd2ad8c0ec263cbd70f860e0e15301626528
green-code-developer <complexcodealgorithm@gmail.com>
2020-09-15 19:19:46 +0530

    API call done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+// instead of 3 imports we can have only one like:
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
+import { fetchData } from './api';
+
+
+
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+  
+
+
   render() { 
     return (
-      <h1> corona tracker </h1>
+        <div className={styles.container} >
+        <Cards />
+        <CountryPicker />
+        <Chart />
+          
+        
+        </div>
+      
       );
   }
 }
