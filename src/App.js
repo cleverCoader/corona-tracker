a33de7c733aaf66fca74d5cb3ad5fddceca96084
green-code-developer <complexcodealgorithm@gmail.com>
2020-09-15 23:29:54 +0530

    Done with cards but data is not from API

--- src/App.js
+++ src/App.js
@@ -8,26 +8,41 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
-
 import { fetchData } from './api';
+
 
 
 
 
 class App extends React.Component {
 
-  async componentDidMount() {
-    const data = await fetchData();
+  state = {
 
-    console.log(data);
+    data:{},
+    
+  }
+
+
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });  
+
+    console.log(this.state.data);
+    
   }
   
 
 
   render() { 
+    const { data } = this.state;
+
+
+
+
     return (
         <div className={styles.container} >
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
           
--- src/api/index.js
+++ src/api/index.js
@@ -5,19 +5,13 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
 
-        const { data:{conformed,recovered,deaths,lastUpdate} } = await axios.get(url);        // before it was: const response = await axios.get(url);
-                                                     
-        const modifiedData = {
-            conformed,                
-            recovered ,           
-            deaths,                 
-            lastUpdate,               
+        const { data:{conformed,recovered,deaths,lastUpdate} } = await axios.get(url);        // before it was: const response = await axios.get(url);                                            
+       
+        //const modifiedData = { conformed, recovered, deaths, lastUpdate }
 
-        }
-
-        
-
-        
+        //return modifiedData;
+        return { conformed, recovered, deaths, lastUpdate } ;
+            
     } catch (error) {
         
     }
